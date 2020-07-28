c207c0230ad1574d3062d0450593e24040602487
Max Verhoef <max.verhoef0@gmail.com>
2020-07-29 09:43:36 +1000

    update

---
+++ postASurvey.js
@@ -0,0 +1,38 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      
+      var user = firebase.auth().currentUser;
+        
+        if(user != null) {
+
+        }
+  
+    } else {
+      // No user is signed in.
+      window.open("login.html","_self");
+    }
+});
+
+function add() {
+    let link = document.getElementById("googleform_field").value;
+    let title = document.getElementById("title_field").value;
+    let description = document.getElementById("description_field").value;
+    let length = document.getElementById("length_field").value;
+    if(link == "") {
+        document.getElementById("addError").style.color = "red";
+        document.getElementById("addError").innerHTML = "Please copy your link from google forms";
+    } else if(title == "") {
+        document.getElementById("addError").style.color = "red";
+        document.getElementById("addError").innerHTML = "Please add a title";
+    } else if(description == "") {
+        document.getElementById("addError").style.color = "red";
+        document.getElementById("addError").innerHTML = "Please add a description";
+    } else if(length == "") {
+        document.getElementById("addError").style.color = "red";
+        document.getElementById("addError").innerHTML = "Please add the survey length";
+    } else{
+        document.getElementById("addError").style.color = "black";
+        document.getElementById("addError").innerHTML = "Your survey has been uploaded";
+    }
+}
